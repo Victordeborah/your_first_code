6fefdd349d0617e2ef7e792bb9726072149c7fcd
Odubunmi <odubunmiolawale@gmail.com>
2017-01-07 17:45:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
-int main()
+#include <stdio.h>
+int main(void)
 {
-  int i;
-  for (i = 0; i < 10; i++)
+  for (int i = 0; i < 32; i++)
   {
-    printf("Holberton\n");
+    printf("Hello, Holberton\n");
   }
   return 0;
 }
